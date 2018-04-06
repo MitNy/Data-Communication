44689799b5da241c22f6fb802253bcf537409c25
MitNy <leemj314@gmail.com>
2018-04-06 20:14:21 +0900

    DC_05
    UDP socket communication

--- dc_05/server.py
+++ dc_05/server.py
@@ -1,5 +1,6 @@
 # 201602038 이미진
 import socket
+import struct
 
 ip_address = '127.0.0.1'
 port_number = 3333
@@ -7,11 +8,38 @@ port_number = 3333
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_sock.bind((ip_address, port_number))
 print("Server socket open...")
+print("---------------------------------------------------------------------------")
+while True :
+	print("Listening...")
+	data,addr = server_sock.recvfrom(5000)
+	de_msg = data.decode()
+	type_check = de_msg[0]
+	convert_msg=""
+	print("Type of Message : "+type_check)
+	print("Received Message from client : "+de_msg[1:])
+	
+	if int(type_check) > 3 :
+		print("Type does not exist.")
+	if type_check == "0":
+		convert_msg = de_msg[1:].upper()
+		print("Converted Message : "+convert_msg)
+		server_sock.sendto(convert_msg.encode(), addr)
+		print("Send to Client Converted Message .....")
+	if type_check == "1":
+		convert_msg = de_msg[1:].lower()
+		print("Converted Message : "+convert_msg)
+		server_sock.sendto(convert_msg.encode(), addr)
+		print("Send to Client Converted Message .....")
+	if type_check == "2":
+		convert_msg = de_msg[1:].swapcase()
+		print("Converted Message : "+convert_msg)
+		server_sock.sendto(convert_msg.encode(), addr)
+		print("Send to Client Converted Message .....")
+	if type_check == "3":
+		convert_msg = ''.join(reversed(de_msg[1:]))
+		print("Converted Message : "+convert_msg)
+		server_sock.sendto(convert_msg.encode(), addr)
+		print("Send to Client Converted Message .....")
 
-print("Listening...")
-data,addr = server_sock.recvfrom(5000)
-print("Received Message from client : "+data.decode())
 
-server_sock.sendto(data, addr)
-print("Send message to client back...")
-
+	print("---------------------------------------------------------------------------")
