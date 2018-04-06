387f8ae8df5dbceee42ce7e7294e7d5330630f98
MitNy <leemj314@gmail.com>
2018-04-06 20:47:19 +0900

    DC_05
    UDP socket communication

--- dc_05/server.py
+++ dc_05/server.py
@@ -1,6 +1,5 @@
 # 201602038 이미진
 import socket
-import struct
 
 ip_address = '127.0.0.1'
 port_number = 3333
@@ -18,28 +17,31 @@ while True :
 	print("Type of Message : "+type_check)
 	print("Received Message from client : "+de_msg[1:])
 	
-	if int(type_check) > 3 :
-		print("Type does not exist.")
-	if type_check == "0":
-		convert_msg = de_msg[1:].upper()
-		print("Converted Message : "+convert_msg)
-		server_sock.sendto(convert_msg.encode(), addr)
-		print("Send to Client Converted Message .....")
-	if type_check == "1":
-		convert_msg = de_msg[1:].lower()
-		print("Converted Message : "+convert_msg)
-		server_sock.sendto(convert_msg.encode(), addr)
-		print("Send to Client Converted Message .....")
-	if type_check == "2":
-		convert_msg = de_msg[1:].swapcase()
-		print("Converted Message : "+convert_msg)
-		server_sock.sendto(convert_msg.encode(), addr)
-		print("Send to Client Converted Message .....")
-	if type_check == "3":
-		convert_msg = ''.join(reversed(de_msg[1:]))
-		print("Converted Message : "+convert_msg)
-		server_sock.sendto(convert_msg.encode(), addr)
-		print("Send to Client Converted Message .....")
+	if "0" <= type_check <= "3" :
+		if type_check == "0":
+			convert_msg = de_msg[1:].upper()
+			print("Converted Message : "+convert_msg)
+			server_sock.sendto(convert_msg.encode(), addr)
+			print("Send to Client Converted Message .....")
+		if type_check == "1":
+			convert_msg = de_msg[1:].lower()
+			print("Converted Message : "+convert_msg)
+			server_sock.sendto(convert_msg.encode(), addr)
+			print("Send to Client Converted Message .....")
+		if type_check == "2":
+			convert_msg = de_msg[1:].swapcase()
+			print("Converted Message : "+convert_msg)
+			server_sock.sendto(convert_msg.encode(), addr)
+			print("Send to Client Converted Message .....")
+		if type_check == "3":
+			convert_msg = ''.join(reversed(de_msg[1:]))
+			print("Converted Message : "+convert_msg)
+			server_sock.sendto(convert_msg.encode(), addr)
+			print("Send to Client Converted Message .....")
+	else :
+		error_msg = "Type does not exist. So, server can't send to message."
+		server_sock.sendto(error_msg.encode(),addr)
+		print("Type does not exist. Can't send to client.")
 
 
 	print("---------------------------------------------------------------------------")
