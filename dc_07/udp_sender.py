ecb85efff79d27f75e1bf8185faceefe96b39568
MitNy <leemj314@gmail.com>
2018-05-06 18:22:21 +0900

    UDP file transfer
    UDP file transfer

--- dc_07/udp_sender.py
+++ dc_07/udp_sender.py
@@ -1,6 +1,7 @@
 import socket
 import os
 import sys
+import hashlib
 
 # sender 요구 사항
 # checksum 20 byte / sequence number 1 byte / data 1024 byte
@@ -9,6 +10,14 @@ import sys
 
 receiverIP = '127.0.0.1'
 receiverPort = 2345
+
+def sha1generator(seqNumber,fileData):
+	encode_number = seqNumber.to_bytes(1,byteorder="big")
+
+	h = hashlib.sha1()
+	h.update(encode_number+fileData)
+	return h.digest()
+
 
 while True:
 	buffer = []
@@ -20,11 +29,14 @@ while True:
 	input_file = input("Input file : ")
 	padding_name = input_file.ljust(15,)
 	data_size = 0	
+	sequence_number = 0
+
 	with open(input_file,"rb") as f:
 		file_size = os.path.getsize("./"+input_file+"")
 		print("[+] Sending file...")
 		data = f.read(1024)
 		sender_sock.sendto(data,(receiverIP,receiverPort))
+		checksum = sha1generator(sequence_number,data)
 
 		f.close()
 	print("Send Message to Server...")
