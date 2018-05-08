6ae9ea473bc76e684edb381e8cc6fbb1780f0de1
MitNy <33439062+MitNy@users.noreply.github.com>
2018-05-09 06:45:36 +0900

    UDP file transfer
    UDP file transfer

--- dc_07/udp_sender.py
+++ dc_07/udp_sender.py
@@ -2,50 +2,74 @@ import socket
 import os
 import sys
 import hashlib
+import struct
+import time
 
 # sender 요구 사항
 # checksum 20 byte / sequence number 1 byte / data 1024 byte
 # sequence number : 프레임 순서 보장, 0-1
 # checksum : 프레임 무결성 보장, sequence number+file data sha1 encode
+# sender는 패킷을 전송할 때마다 타이머를 구동하고 타이머가 만료되기 전에 확인 응답이 도착하면 타이머는 정지되고 다음 패킷을 보낸다.
+# 데이터통신 교과서 참고
 
 receiverIP = '127.0.0.1'
 receiverPort = 2345
 
 def sha1generator(seqNumber,fileData):
 	encode_number = seqNumber.to_bytes(1,byteorder="big")
-
 	h = hashlib.sha1()
 	h.update(encode_number+fileData)
 	return h.digest()
 
 
 while True:
-	buff = []
 	sender_sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 	print("[+] Connected with Receiver")
 	print("Receiver IP = ",receiverIP)
 	print("Receiver Port = ",receiverPort)
-	print("------------------------------------------------------")
+	print("-----------------------------------")
 	input_file = input("Input file : ")
-	padding_name = input_file.ljust(15,)
-	data_size = 0	
-	sequence_number = 0
+	padding_name = input_file.ljust(11,)
+
+	
 
 	with open(input_file,"rb") as f:
+		f.seek(0)
 		file_size = os.path.getsize("./"+input_file+"")
-		print("[+] Sending file...")
-		data = f.read(1024)
-		while(data):
-			if not data:
-				break
-			if sequence_number == 0 :
-				encode_seqNumber = sequence_number.to_bytes(1,byteorder="big")
-				checksum = sha1generator(sequence_number,data)
-				buff = padding_name.encode()+checksum+encode_seqNumber+data 
-				sender_sock.sendto(padding_name.encode()+checksum+encode_seqNumber+data,(receiverIP,receiverPort))
-				print("receive : ",sender_sock.recv(10))			
+		kb_size = file_size/1024
+		padding_size = str(int(kb_size)).rjust(4,);
+		sequence_number = 0
+		data_size = 0
 
-		f.close()
-	print("Send Message to Server...")
-	print("------------------------------------------------------")
+		print("[+] Sending file...")
+		# file info transfer
+		info_checksum = sha1generator(sequence_number,"".encode().ljust(1024,))
+		encode_seqNumber = sequence_number.to_bytes(1,byteorder="big")
+		
+		print(info_checksum)
+		sender_sock.sendto((padding_name.encode()+padding_size.encode()+info_checksum+encode_seqNumber).ljust(1060,),(receiverIP,receiverPort))
+
+		ACK = struct.unpack("!i",sender_sock.recv(4))[0]
+		print(ACK)
+		sequence_number = ACK
+		if ACK == 1:
+			data = f.read(1024)
+			while (data):
+				data_size += len(data)
+				sequence_number = data[36]
+				encode_number = sequence_number.to_bytes(1,byteorder="big")
+				checksum = sha1generator(sequence_number,data)
+				sender_sock.sendto(padding_name.encode()+padding_size.encode()+checksum+encode_number+data,(receiverIP,receiverPort))
+				data = f.read(1024)
+				print(data_size,"/",file_size," , ","{0:.2f}".format((data_size/float(file_size))*100),"%")
+				try:
+					sender_sock.settimeout(1)
+					ACK = sender_sock.recv(4)
+					sequence_number = struct.unpack("!1i",sender_sock.recv(4))[0]
+				except:
+					continue
+				if data_size == file_size:
+					break								
+	f.close()
 	sender_sock.close()
+	print("-----------------------------------")
